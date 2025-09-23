1211b6ec71299b8c8686ba42d4fbdbe39449b710
Von <voncarcha@gmail.com>
2025-09-23 16:19:48 +0800

    update about

--- src/components/home/WhoWeAreSection/index.tsx
+++ src/components/home/WhoWeAreSection/index.tsx
@@ -5,8 +5,11 @@ import SvgBackground from './SvgBackground';
 
 const Who = () => {
   return (
-    <article className="w-full bg-primary px-[15px] py-[50px] overflow-hidden" id="about">
-      <section className="mx-auto flex gap-[20px] max-w-[1200px] items-center justify-between flex-col lg:flex-row">
+    <article
+      className="w-full overflow-hidden bg-primary px-[15px] py-[50px]"
+      id="about"
+    >
+      <section className="mx-auto flex max-w-[1200px] flex-col items-center justify-between gap-[20px] lg:flex-row">
         <header className="relative">
           <span className="absolute left-[-30px] top-[-30px]">
             <SvgBackground />
@@ -15,25 +18,46 @@ const Who = () => {
           <h2 className="max-w-[700px] text-[40px] font-bold leading-tight text-white">
             Who we are
           </h2>
+
           <p className="mt-[20px] max-w-[700px] text-[20px] text-white">
-            We’re not a traditional agency—we’re a collective of senior
-            designers and engineers who’ve come together to do things
-            differently.
+            We’re a lean collective of designers and developers who believe in
+            doing things differently. No middle layers, no fluff—just direct
+            collaboration between clients and the people building their
+            products.
           </p>
           <p className="mt-[20px] max-w-[700px] text-[20px] text-white">
-            After years of working inside bloated agencies, slow-moving
-            enterprises, and disconnected teams, we decided to build something
-            better: A lean, expert-driven studio where the people who speak with
-            clients are the same ones who design and ship the work.
+            Led by{' '}
+            <a
+              href="https://voncarcha.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-bold hover:underline"
+            >
+              Von Carcha
+            </a>
+            , a front-end developer and web designer with 10+ years of
+            experience in React, Next.js, WordPress, and UI/UX, we deliver
+            clean, responsive, and user-focused solutions. Supporting client
+            relationships is{' '}
+            <a
+              href="https://www.linkedin.com/in/hellomaplrcarcha/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-bold hover:underline"
+            >
+              Maria Carcha
+            </a>
+            , our Client Success Manager, ensuring smooth communication and
+            successful outcomes.
           </p>
           <p className="mt-[20px] max-w-[700px] text-[20px] text-white">
-            We believe in decentralizing the product workforce—no unnecessary
-            layers, no middlemen, no fluff. Just real collaboration between
-            clients and the people building their products.
+            Together, we bridge the gap between design, development, and client
+            needs—creating digital experiences that are lean, impactful, and
+            built to last.
           </p>
         </header>
 
-        <picture className="flex max-w-[480px] items-center justify-center mt-[30px] md:mt-0">
+        <picture className="mt-[30px] flex max-w-[480px] items-center justify-center md:mt-0">
           <Image
             src="/images/home/hero-2.png"
             alt="bg-access"
